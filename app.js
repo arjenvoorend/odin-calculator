a7b1fe29c1b7d12af089e82132a003ef9d926bf6
Arjen Voorend <a.o.voorend@gmail.com>
2022-07-05 15:36:17 +0200

    Add calculator functionality

--- app.js
+++ app.js
@@ -1,43 +1,130 @@
-function add(a, b) {
-  console.log(a + b);
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
 };
 
-function subtract(a, b) {
-  console.log(a - b);
+
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
+
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    // Overwrite `displayValue` if the current value is '0' otherwise append to it
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    console.log(calculator);
+  }
+}
+
+
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = '0.'
+    calculator.waitingForSecondOperand = false;
+    return;
+  }
+
+  // if the 'displayValue' property does not contain a decimal point
+  if (!calculator.displayValue.includes(dot)) {
+    // Append the decimal point
+    calculator.displayValue += dot;
+  }
+}
+
+
+function handleOperator(nextOperator) {
+  // Destructure the properties on the calculator object
+  const { firstOperand, displayValue, operator } = calculator
+  // `parseFloat` converts the string contents of `displayValue`
+  // to a floating-point number
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    console.log(calculator);
+    return;
+  }
+  // verify that `firstOperand` is null and that the `inputValue`
+  // is not a `NaN` value
+  if (firstOperand === null && !isNaN(inputValue)) {
+    // Update the firstOperand property
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function resetCalculator() {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === '+') {
+    return firstOperand + secondOperand;
+  } else if (operator === '-') {
+    return firstOperand - secondOperand;
+  } else if (operator === 'x') {
+    return firstOperand * secondOperand;
+  } else if (operator === '/') {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+
+function updateDisplay() {
+  const display = document.querySelector('.calculator-screen')
+  display.value = calculator.displayValue;
 };
 
-function multiply(a, b) {
-  console.log(a * b);
-};
 
-function divide(a, b) {
-  console.log(a / b);
-};
+updateDisplay();
 
-function operate(operator, a, b) {
-  switch (operator) {
-    case 'add':
-      add(a, b);
+
+const keys = document.querySelector('.calculator-btns')
+keys.addEventListener('click', event => {
+  const { target } = event;
+  const { value } = target;
+  if (!target.matches('button')) {
+    return;
+  }
+
+  switch (value) {
+    case '+':
+    case '-':
+    case 'x':
+    case '/':
+    case '=':
+      handleOperator(value);
       break;
-
-    case 'subtract':
-      subtract(a, b);
+    case '.':
+      inputDecimal(value);
       break;
-
-    case 'multiply':
-      multiply(a, b);
+    case 'all-clear':
+      resetCalculator();
       break;
+    default:
+      // check if the key is an integer
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
+  }
 
-    case 'divide':
-      divide(a, b);
-      break;
-  };
-};
-
-// test cases:
-// operate('add', 1, 2)
-// operate('subtract', 1, 2)
-// operate('multiply', 1, 2)
-// operate('divide', 1, 2)
-
-
+  updateDisplay();
+});
